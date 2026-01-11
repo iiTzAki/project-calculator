64e3e6adab25b29bc5f6124fd1065c700993c966
Aki <210830810+iiTzAki@users.noreply.github.com>
2026-01-12 01:36:33 +0500

    add, refactor: make the UI interactive with js
    Basic working operations like addition,
    subtraction, multiplication and division. Some
    changes made to html and css to fit this feature
    of the project.

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
-function add(num1, num2) { 
-    return num1 + num2;
-};
+let num1 = null;  
+let num2 = null;
+let operator = "";
 
-function subtract(num1, num2) { 
-    return num1 - num2;
-};
 
-function multiply(num1, num2) { 
-    return num1 * num2; 
-};
+const add = (num1, num2) => num1 + num2; 
+const subtract = (num1, num2) => num1 - num2; 
+const multiply = (num1, num2) => num1 * num2; 
+const divide = (dividend, divisor) => {
+    return divisor != 0 ? dividend / divisor : "Error"
+};  
 
-function divide(dividend, divisor) { 
-    return divisor != 0 ? dividend / divisor : "Error"; 
-}; 
-
-let num1;  
-let num2;
-let operator; 
 
 function operate(operator, num1, num2) { 
     switch(operator) {
@@ -24,9 +17,75 @@ function operate(operator, num1, num2) {
             return add(num1, num2); 
         case '-': 
             return subtract(num1, num2); 
-        case '*': 
+        case 'x': 
             return multiply(num1, num2); 
         case '/': 
             return divide(num1, num2)
     }
 }
+
+function updateNumbers() { 
+
+    const displayContainer = document.querySelector(".display-container"); 
+    const buttonContainer = document.querySelector(".button-container"); 
+
+    buttonContainer.addEventListener("click", (event) => { 
+
+        const target = event.target; 
+        let value = target.textContent;
+
+        if (!target.classList.contains("btn")) return;
+
+        if (target.classList.contains("clear")) { 
+            displayContainer.textContent = "0"; 
+            num1 = null; 
+            num2 = null;
+            operator = ""; 
+            return;
+        }
+
+        if (target.classList.contains("equals-to")) { 
+            if (num1 !== null && num2 !== null && operator !== "") { 
+                const result = operate(operator, parseFloat(num1), parseFloat(num2))
+
+                displayContainer.textContent = result; 
+                console.log(result)
+                num1 = result; 
+                num2 = null; 
+                operator = "";
+                return;
+            }
+        }
+
+        if (target.classList.contains("number")) { 
+            if (operator) { 
+                displayContainer.textContent == "0" 
+                ? displayContainer.textContent = value
+                : displayContainer.textContent += value
+
+                num2 = displayContainer.textContent
+            } 
+            else { 
+                displayContainer.textContent == "0"
+                ? displayContainer.textContent = value
+                : displayContainer.textContent += value
+
+                num1 = displayContainer.textContent
+            }      
+        } 
+
+        if (target.classList.contains("operator")) {  
+            displayContainer.textContent = "0"
+            operator = value; 
+        }
+    
+
+        console.log(num1)
+        console.log(num2)
+        console.log(operator)
+
+    })
+
+}
+
+updateNumbers()
